25d0e89cf8cd4329638fbfcca4ab08379254dd8f
mr.Slink <mrslinkx8@gmail.com>
2017-09-02 23:45:48 +0300

    Добавил поддержку mysqli
    Но не знаю, работает ли.

--- report.class.php
+++ report.class.php
@@ -3,11 +3,11 @@
  * Класс создания базы отчетов
  * Тип данных: json
  * Разделитель: \r\n
- * Версия скрипта: 1.0.7
+ * Версия скрипта: 1.0.8 beta
  * Автор: mr.Slink
  *
  * Описание скрипта:
- * Этот скрипт преобразует базу данных MySQL в формат Json 
+ * Этот скрипт преобразует базу данных MySQLi в формат Json 
  * и сохраняет ее на сервере (в панели). Файл можно свободно 
  * просматривать без потери скорости и восстанавливать данные.
  *
@@ -15,51 +15,52 @@
  */
  /*
     Как это использовать:
-    //# СОЗДАНИЕ ФАЙЛА ОТЧЁТА #
-    //Объявите класс:
-    $report = new Report();
+        //# СОЗДАНИЕ ФАЙЛА ОТЧЁТА #
+            //Объявите класс:
 
-    //Укажите параметры:
-    include("db.php);
-    $report->archive_path        = "./archiv/";
-    $report->atc_enable          = false;
-    $report->encrypt_enable      = false;
-    $report->cleardb_enable      = false;
-    $report->cleardb_ignore      = ['settings', 'pass']; 
-    $report->db_name             = $dbName;
+            $report = new Report();
 
-    //Запустите процесс создания файла:
-    $report->write();
+            //Укажите параметры:
+            include("db.php);
+            $report->archive_path        = "./archiv/";
+            $report->atc_enable          = false;
+            $report->encrypt_enable      = false;
+            $report->cleardb_enable      = false;
+            $report->use_mysqli          = true;
+            $report->cleardb_ignore      = ['settings', 'pass']; 
+            $report->db_name             = $dbName;
 
-    //# ЧТЕНИЕ ФАЙЛА ОТЧЁТА #
-    //Объявите класс:
-    $report = new Report();
+            //Запустите процесс создания файла:
+            $report->write();
 
-    //Запустите процесс чтения файла в массив:
-    $array = $report->read("filename.db", "zakaz"); //Where "zakaz" is name of the table
-    foreach($array as $i){..}
+        //# ЧТЕНИЕ ФАЙЛА ОТЧЁТА #
+            //Объявите класс:
+            $report = new Report();
 
-    //# ВОССТАНОВЛЕНИЕ БАЗЫ ИЗ ОТЧЁТА #
-    //Объявите класс:
-    $report = new Report();
-
-    //Запустите процесс чтения файла:
-    $report->cleardb_enable = true;             //Включите функцию очистки базы
-    $restore->cleardb();                        //Запустите ее
-    $array = $report->restore("filename.db");   //Восстановите базу
-    
-    Чистка - не обязательный процесс, но может вызвать ошибку, если информация в базе присуствует.
+            //Запустите процесс чтения файла в массив:
+            $array = $report->read("filename.db", "zakaz"); //Where "zakaz" is name of the table
+            foreach($array as $i){..}
+        
+        //# ВОССТАНОВЛЕНИЕ БАЗЫ ИЗ ОТЧЁТА #
+            //Объявите класс:
+            $report = new Report();
+            
+            //Запустите процесс чтения файла:
+            $report->cleardb_enable = true;             //Включите функцию очистки базы
+            $restore->cleardb();                        //Запустите ее
+            $array = $report->restore("filename.db");   //Восстановите базу
+        
+        Чистка - не обязательный процесс, но может вызвать ошибку, если информация в базе присуствует.
     */
 Class Report{
-
     //Параметры по-умолчанию
     Public $archive_path        = "./archiv/";          //Путь к папке архива
     Public $atc_enable          = true;                 //Автоматическое создание эскизов
     Public $encrypt_enable      = false;                //Шифрование файлов
     Public $cleardb_enable      = false;                //Фкнция очистки базы
     Public $cleardb_ignore      = ['settings', 'pass']; //Таблицы, которые будут проигнорированы при очистки
+    Public $use_mysqli          = true;                 //Использовать mysqli
     Public $db_name             = "dancefile";          //Имя текущей базы
-
     /*
     * Код:
     */
@@ -67,10 +68,16 @@ Class Report{
     Public $table_list; 
     
     Public Function __construct(){
-         $result = mysql_list_tables($this->db_name);
-
-        while ($table = mysql_fetch_row($result)) {
-            $this->table_list[] = $table[0];
+        if($this->use_mysqli == false){
+            $result = mysql_list_tables($this->db_name);
+            while ($table = mysql_fetch_row($result)) {
+                $this->table_list[] = $table[0];
+            }
+        } else {
+            $result = mysqli_query($mysqli, "SHOW TABLES");
+            while($table = mysqli_fetch_array($result)){
+                $this->table_list[] = $table[0];
+            }
         }
         $this->table_list[] = "thumbnail";
     }
@@ -82,18 +89,26 @@ Class Report{
      */
     Public Function cleardb(){
         if($this->cleardb_enable == true){
-            $result = mysql_list_tables($this->db_name);
-
-            while ($table = mysql_fetch_row($result)) {
-                if(!in_array($table[0], $this->cleardb_ignore)){
-                    mysql_query("TRUNCATE TABLE ".$table[0]);
-                }  
+            if($this->use_mysqli == false){
+                $result = mysql_list_tables($this->db_name);
+                while ($table = mysql_fetch_row($result)) {
+                    if(!in_array($table[0], $this->cleardb_ignore)){
+                        mysql_query("TRUNCATE TABLE ".$table[0]);
+                    }  
+                }
+                return true;
+            } else {
+                $result = mysqli_query($mysqli, "SHOW TABLES");
+                while ($table = mysqli_fetch_array($result)) {
+                    if(!in_array($table[0], $this->cleardb_ignore)){
+                        mysqli_query($mysqli, "TRUNCATE TABLE ".$table[0]);
+                    }  
+                }
+                return true;
             }
-            return true;
         }
         return false;
     }
-
     /**
      * Проверка существования файла
      *
@@ -102,7 +117,6 @@ Class Report{
     Public Function report_exists($filename){
        return file_exists($this->archive_path.$filename);
     }
-
     /**
      * Функция восстановления базы данных
      */
@@ -113,12 +127,15 @@ Class Report{
                 foreach($tablex as $key){
                     $name = implode("`, `",array_keys($key));
                     $value = implode("', '",$key);
-                    mysql_query("INSERT INTO `".$table."` (`".$name."`) VALUES ('".$value."')");
+                    if($this->use_mysqli == false){
+                        mysql_query("INSERT INTO `".$table."` (`".$name."`) VALUES ('".$value."')");
+                    } else {
+                        mysqli_query($mysqli, "INSERT INTO `".$table."` (`".$name."`) VALUES ('".$value."')");
+                    }
                 }
             }
         }
     }
-
     /**
      * Получаем путь к архиву
      *
@@ -127,7 +144,6 @@ Class Report{
     Public Function path(){
         return $this->archive_path;
     }
-
     /**
      * Получаем разрешение на создание эскизов
      *
@@ -136,7 +152,6 @@ Class Report{
     Public Function atc(){
         return $this->atc_enable; 
     }
-
     /**
      * Получаем разрешение на шифрование файлов
      *
@@ -145,7 +160,6 @@ Class Report{
     Public Function encrypt(){
         return $this->encrypt_enable;
     }
-
     /**
      * Функция чтения файла отчёта
      *
@@ -165,13 +179,11 @@ Class Report{
             }
             $FileOpen = fopen($this->path().$filename,"r");
             $FileRead = fread($FileOpen, filesize($this->path().$filename));
-
             fclose($FileOpen);
             return $this->decryption(json_decode(explode("\r\n", $FileRead)[$part], true));
         }
         return false;
     }
-
     /**
      * Функция чтения настроек из файла отчёта (из таблицы Settings)
      *
@@ -189,7 +201,6 @@ Class Report{
         }
         return false;
     }
-
     /**
      * Генерация имени файла из текущей даты
      *
@@ -200,7 +211,6 @@ Class Report{
     Public Function create_date($filename){
         return date("d.m.Y", filectime($this->archive_path.$filename));
     }
-
     /**
      * Сохранение эскизов в формате json
      *
@@ -210,15 +220,23 @@ Class Report{
      */
     Public Function thumbnail_creation($array){
         $rows = array();$i=0;
-        while($r = mysql_fetch_assoc($array)) {
-            $i++;
-            $rows[$i]['id'] = $r['id'];
-            $rows[$i]['name'] = $r['name'];
-            $rows[$i]['base64'] = $this->to_base64("./ph/".$r['url']."/".$r['name']);
+        if($this->use_mysqli == false){        
+            while($r = mysql_fetch_assoc($array)) {
+                $i++;
+                $rows[$i]['id'] = $r['id'];
+                $rows[$i]['name'] = $r['name'];
+                $rows[$i]['base64'] = $this->to_base64("./ph/".$r['url']."/".$r['name']);
+            }
+        } else {
+            while($r = mysqli_fetch_assoc($array)) {
+                $i++;
+                $rows[$i]['id'] = $r['id'];
+                $rows[$i]['name'] = $r['name'];
+                $rows[$i]['base64'] = $this->to_base64("./ph/".$r['url']."/".$r['name']);
+            }
         }
         return json_encode($rows);
     }
-
     /**
      * Получение кода эскиза из файла по ID фотографии
      *
@@ -235,7 +253,6 @@ Class Report{
         }   
         return false;
     }
-
     /**
      * Конвертирование изображения в base64
      *
@@ -248,7 +265,6 @@ Class Report{
         $data = file_get_contents($path);
         return 'data:image/' . $type . ';base64,' . base64_encode($data);
     }
-
     /**
      * Шифрование файла
      *
@@ -262,7 +278,6 @@ Class Report{
         }
         return $content;
     }
-
     /**
      * Дешифрование файла
      *
@@ -276,24 +291,48 @@ Class Report{
         }
         return $content;
     }
-
     /**
      * Функция записи файла отчёта
      *
      * @return boolean
      */
     Public Function write(){
+
         require_once "db.php";
-        $result = mysql_list_tables($this->db_name); $Content="";
-        while ($table = mysql_fetch_row($result)) {$Content .= $this->to_json(mysql_query("SELECT * from ".$table[0]))."\r\n";};
-        if($this->atc() == true){$Content .= $this->thumbnail_creation(mysql_query("SELECT * from foto"));};
+
+        if($this->use_mysqli == false){
+            $result = mysql_list_tables($this->db_name); 
+        } else {
+            $result =  mysqli_query($mysqli, "SHOW TABLES");
+        }
+        
+        $Content="";
+
+        if($this->use_mysqli == false){
+            while ($table = mysql_fetch_row($result)) {
+                $Content .= $this->to_json(mysql_query("SELECT * from ".$table[0]))."\r\n";
+            };
+        } else {
+            while ($table = mysqlo_fetch_row($result)) {
+                $Content .= $this->to_json(mysqli_query($mysqli, "SELECT * from ".$table[0]))."\r\n";
+            };
+        };
+        
+        if($this->atc() == true){
+            if($this->use_mysqli == false){
+                $Content .= $this->thumbnail_creation(mysql_query("SELECT * from foto"));
+            } else {
+                $Content .= $this->thumbnail_creation(mysqli_query($mysqli, "SELECT * from foto"));    
+            };
+        };
+        
+        
         $FileOpen = fopen($this->path().date("d").rand(1000,9999).date("m").".db","w");
         fwrite($FileOpen, $Content);
         fclose($FileOpen);
         $this->cleardb();
         return $Content;
     }
-
     /**
      * Функция конвертирования данных в формат json
      *
@@ -303,8 +342,15 @@ Class Report{
      */
     Private Function to_json($array){
         $rows = array();
-        while($r = mysql_fetch_assoc($array)) {
-            $rows[] = $r;
+
+        if($this->use_mysqli == false){
+            while($r = mysql_fetch_assoc($array)) {
+                $rows[] = $r;
+            }
+        } else {
+            while($r = mysqli_fetch_assoc($array)) {
+                $rows[] = $r;
+            }
         }
         return $this->encryption(json_encode($rows));
     }
